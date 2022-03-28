1486eb4a1d73af416d24c1556346de9be962eb4c
Elroy Kanye <elroykanye@gmail.com>
2022-03-28 22:26:58 +0100

    generics, traits and lifetimes

--- r06-modules/src/main.rs
+++ r06-modules/src/main.rs
@@ -1,5 +1,38 @@
+use std::fs::File;
+use std::io::ErrorKind;
 
+fn ex01_panicked() {
+    panic!("crash and burn")
+}
+
+fn ex02_result() {
+    let f = match File::open(("hell0.txt")) {
+        Ok(file) => file,
+        Err(err) => {
+            panic!("There was a problem opening the file: {:?}", err);
+        }
+    };
+}
+
+fn ex03_result_err() {
+    let filename = "hel.txt";
+    let f = match File::open(filename) {
+        Ok(file) => file,
+        Err(ref err) if err.kind() == ErrorKind::NotFound => {
+            match File::create(filename) {
+                Ok(new_file) => new_file,
+                Err(err) => {
+                    panic!("Tried to create file but there was a problem: {:?}", err);
+                }
+            }
+        },
+        Err(err) => {
+            panic!("There was a problem opening the file: {:?}", err);
+        }
+    };
+
+}
 
 fn main() {
-    println!("Hello, world!");
+    ex03_result_err();
 }
