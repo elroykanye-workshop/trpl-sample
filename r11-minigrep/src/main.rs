39e5b6f1c5b7b9cdc49a5c07554cbbfafad27dc5
Elroy Kanye <elroykanye@gmail.com>
2022-05-22 09:39:30 +0100

    complete minigrep

--- r11-minigrep/src/main.rs
+++ r11-minigrep/src/main.rs
@@ -1,3 +1,19 @@
+use std::{env, process};
+use r11_minigrep::{GrepArgs, runner};
+
 fn main() {
-    println!("Hello, world!");
+    // collect the arguments and store in a vec
+    let args: Vec<String> = env::args().collect();
+
+    let grep_args = GrepArgs::new(&args)
+        .unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+            process::exit(1);
+    });
+
+    if let Err(e) = runner(grep_args) {
+        eprintln!("Application error: {}", e);
+        process::exit(1);
+    }
+    println!("{:?}", args);
 }
